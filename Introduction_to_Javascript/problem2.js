6a0990cd7ca1c052246c0296e2bd32fc5e498263
abhipathak1 <123934582+abhipathak1@users.noreply.github.com>
2023-01-31 15:39:26 +0530

    js_variable_files

---
+++ Introduction_to_Javascript/problem2.js
@@ -0,0 +1,6 @@
+let name = "Abhay Pathak"
+console.log(name);
+ name = "shiv kr Pathak"
+console.log(name);
+ name = "Pratima Pathak"
+console.log(name);
---
+++ Introduction_to_Javascript/problem4.js
@@ -0,0 +1,21 @@
+let name="Abhay Pathak"
+let school="Masai School"
+let subject1="Physics"
+let subject2="Chemistry"
+let subject3="Biology"
+let s1 = 70
+let s2 = 80
+let s3 = 90
+let class12 = "Grade"
+let sn = 12
+let rn ="Roll NO"
+let rn1=11
+console.log(" --",school,"--")
+console.log("|",name,"     |",)
+console.log("|",class12,"-",sn,"       |",)
+console.log("|",rn,"-",rn1,"     |",)
+console.log("|",subject1,"-",s1,"     |",)
+console.log("|",subject1,"-",s2,"     |",)
+console.log("|",subject3,"-",s3,"     |",)
+let percent= ((s1+s2+s3)*100)/300
+console.log("|","percentage","=",percent,"%","|",)
