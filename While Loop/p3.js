5bbe07f9e6f3bb31bc082cc960b2181f8fdc68d0
abhipathak1 <123934582+abhipathak1@users.noreply.github.com>
2023-02-06 11:39:04 +0530

    W_loop

---
+++ While Loop/p3.js
@@ -0,0 +1,5 @@
+let a=3;
+while(a<=30){
+  console.log(a);
+  a=a+3;
+}
---
+++ While Loop/p4.js
@@ -0,0 +1,15 @@
+let a=1;
+let count=0;
+let sum=0;
+while(a<=100){
+  if(a%2==0){
+    count++;
+    sum=a+sum;
+  }
+  a++
+ 
+ 
+}
+let average = (sum/count);
+console.log(average)
+  
